aff548fb11db81254e761184df2d624c3201d938
Charles <charles.barbier.web@gmail.com>
2015-11-10 13:57:11 +0100

    Email Fonctionnel depuis Form

--- send.php
+++ send.php
@@ -1,33 +1,39 @@
 <?php
 require_once 'vendor/autoload.php';
 
-$mail = new PHPMailer;
+if(isset($_POST['submit'])){
+    $dest_mail = htmlspecialchars(trim($_POST['email']));
+    var_dump($dest_mail);
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';                 // Specify main and backup server
-$mail->Port = 465;                                    // Set the SMTP port
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'EMAIL A RAJOUTER';                // SMTP username
-$mail->Password = 'MDP A RAJOUTER';                  // SMTP password
-$mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
+    $mail = new PHPMailer;
 
-$mail->From = 'CA JE SAIS PAS SI CA MARCHE';
-$mail->FromName = 'Your From name';
-$mail->AddAddress('ADRESSE A RAJOUTER', 'Benjamin Darcet');  // Add a recipient
-               // Name is optional
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';                 // Specify main and backup server
+    $mail->Port = 465;                                    // Set the SMTP port
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'nospoilwithlove@gmail.com';                // SMTP username
+    $mail->Password = 'jorbencha';                  // SMTP password
+    $mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
 
-$mail->IsHTML(true);                                  // Set email format to HTML
+    $mail->From = 'nospoilwithlove@gmail.com';
+    $mail->FromName = 'Your From name';
+    $mail->AddAddress($dest_mail);  // Add a recipient
+                   // Name is optional
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <strong>in bold!</strong>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->IsHTML(true);                                  // Set email format to HTML
 
-if(!$mail->Send()) {
- echo 'Message could not be sent.';
- echo 'Mailer Error: ' . $mail->ErrorInfo;
- exit;
-}
+    $mail->Subject = 'Petit cadeau d\'un ami';
+    $mail->Body    = 'This is the HTML message body <strong>in bold!</strong>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-echo 'Message has been sent';
+    if(!$mail->Send()) {
+     echo 'Message could not be sent.';
+     echo 'Mailer Error: ' . $mail->ErrorInfo;
+     exit;
+    }
+
+    echo 'Message has been sent';
+
+  }
 
 ?>
